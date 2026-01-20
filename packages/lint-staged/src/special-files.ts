f65f34507738a8dd086b29e382fcc08aaa53a73a
Lukas Riha <lukas.riha@cleevio.com>
2026-01-20 17:04:17 +0100

    style(actions): add github actions

--- packages/lint-staged/src/special-files.ts
+++ packages/lint-staged/src/special-files.ts
@@ -2,5 +2,4 @@ import type { Configuration } from 'lint-staged';
 
 export const specialFiles = {
   './.changeset/*.{json,md}': ['biome check --write'],
-  './.github/**/*.{yml,yaml}': ['biome check --write'],
 } satisfies Configuration;

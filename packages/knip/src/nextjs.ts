6db35b986fe116239e5230c982ad2f857cb5159b
Lukas Riha <lukas.riha@cleevio.com>
2026-02-23 20:09:59 +0100

    feat(knip): rewrite to knip configs

--- packages/knip/src/nextjs.ts
+++ packages/knip/src/nextjs.ts
@@ -2,7 +2,7 @@ import type { KnipConfig } from 'knip';
 
 export const nextjsConfig = {
   ignore: ['src/assets/components/icons/**'],
-  ignoreDependencies: ['@changesets/cli', 'orval', 'postcss', 'tailwindcss'],
+  ignoreDependencies: ['@changesets/cli', 'orval', 'postcss', 'tailwindcss', '@cleeviox/knip'],
   ignoreExportsUsedInFile: true,
   next: {
     entry: ['next.config.{js,ts}', 'src/app/**/*.{ts,tsx}', 'src/middleware.{ts,js}', 'src/module/i18n/*.ts'],

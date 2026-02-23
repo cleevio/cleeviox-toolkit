78d54eea564d2b075ec3e8a1ede004079470d0c1
Lukas Riha <lukas.riha@cleevio.com>
2026-02-23 20:01:00 +0100

    feat(knip): rewrite to knip configs

--- packages/knip/src/monorepo-root.ts
+++ packages/knip/src/monorepo-root.ts
@@ -1,3 +1,14 @@
 import type { KnipConfig } from 'knip';
 
-export const monorepoRootConfig = {} satisfies KnipConfig;
+export const monorepoRootConfig = {
+  workspaces: {
+    '.': {
+      entry: 'scripts/*.js',
+      project: 'scripts/**/*.js',
+    },
+    'packages/*': {
+      entry: '{index,cli}.ts',
+      project: '**/*.ts',
+    },
+  },
+} satisfies KnipConfig;
